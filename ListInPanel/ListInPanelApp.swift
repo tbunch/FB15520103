686bd199aaa78292b8b17b7503de3e0dfd977cf7
Tom Bunch <tom@omnigroup.com>
2024-10-16 14:43:17 -0700

    Initial Commit

---
+++ ListInPanel/ListInPanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListInPanelApp.swift
+//  ListInPanel
+//
+//  Created by Tom Bunch on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ListInPanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListInPanelTests/ListInPanelTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListInPanelTests.swift
+//  ListInPanelTests
+//
+//  Created by Tom Bunch on 10/16/24.
+//
+
+import Testing
+@testable import ListInPanel
+
+struct ListInPanelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
